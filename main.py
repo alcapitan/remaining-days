84860bac90e271ffe3d6b43e779f33fc42ae71fe
Al Capitan <alexandre.boyer29@gmail.com>
2023-05-04 12:30:10 +0000

    Final work for today

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import datetime
 from dateutil.relativedelta import relativedelta
 import time
 
+lang = "fr"
+
+texts = {
+    "en": {
+        "sentence": "Days before ",
+        "in": "in",
+        "days": "days",
+        "Retirement": "Retirement",
+    },
+    "fr": {
+        "sentence": "Jours avant ",
+        "in": "dans",
+        "days": "jours",
+        "Retirement": "Retraite",
+    },
+}
+
 # Default date if date reading failed
 default_date = datetime.date(1982, 7, 1)
 retirement_age = 67
@@ -13,7 +30,6 @@ if arduino_mode:
     import JeulinLib
     JeulinLib.Connect("COM3")
     JeulinLib.LCD_RGB_Clear()
-    JeulinLib.LCD_RGB_SetCusor(0, 0)
     JeulinLib.LCD_RGB_SetColor(255, 153, 153)
 
 
@@ -25,20 +41,26 @@ def read_date_file():
     return date
 
 
-def run():
+def run(event_name):
     while True:
         today = datetime.date.today()
         try:
             interval_days = read_date_file() - today
         except:
+            event_name = texts[lang]["retirement"]
             interval_days = default_date - today
         interval_days = interval_days.days
-        print(interval_days)
+        print(event_name + " " + texts[lang]["in"])
+        print(str(interval_days) + " " + texts[lang]["days"])
         if arduino_mode:
-            JeulinLib.LCD_RGB_Write(interval_days)
+            JeulinLib.LCD_RGB_SetCusor(0, 0)
+            JeulinLib.LCD_RGB_Write(event_name + " " + texts[lang]["in"])
+            JeulinLib.LCD_RGB_SetCusor(1, 0)
+            JeulinLib.LCD_RGB_Write(
+                str(interval_days) + " " + texts[lang]["days"])
         # time.sleep(1)
         time.sleep(86400)  # 24 hours in seconds
 
 
 # print(read_date_file())
-run()
+run("Bac philo")
